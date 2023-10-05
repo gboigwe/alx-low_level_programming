de11678d8660215f5d87269b4706fd087133b080
gboigwe <geakande@gmail.com>
2023-10-05 15:20:59 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Descrpitio: This program prints exactly a line content using the put function
+ *
+ * Return: value 0
+ */
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
