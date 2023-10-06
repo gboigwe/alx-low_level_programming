dc8276fa59926a9f2d06bb9cf448a78886a9c917
gboigwe <geakande@gmail.com>
2023-10-06 15:58:02 -0700

    Both

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+	char ALPHA = 'A';
+
+	while (alpha == 'z' && ALPHA =='Z')
+	{
+		putchar(alpha);
+		putchar(ALPHA);
+		alpha++;
+		ALPHA++;
+	}
+	return (0);
+}
