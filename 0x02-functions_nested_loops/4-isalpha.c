780829c1366a08348d9f7e250a3286ab56f4856d
gboigwe <geakande@gmail.com>
2023-10-10 14:26:53 -0700

    Aga

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if it is alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
