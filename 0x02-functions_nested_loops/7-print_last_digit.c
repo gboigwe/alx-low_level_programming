a7c3515eb5b50b68ab92c45102b0f1d373e7c350
gboigwe <geakande@gmail.com>
2023-10-10 17:10:07 -0700

    tem

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
