046a7f0af78d6b831a5d21f1fe1ed9994b9e3e1c
gboigwe <geakande@gmail.com>
2023-10-11 15:13:17 -0700

    Done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	int i;
+	int i, z;
 
 	while  (i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("%d\n", i);
+			z += i;
 		}
 		i++;
 	}
+	printf("%d\n", i);
 	return (0);
 }
