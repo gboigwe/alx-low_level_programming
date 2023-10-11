281708e762ee48d7171fa755ae3489167b243d9d
gboigwe <geakande@gmail.com>
2023-10-11 15:00:51 -0700

    G

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,13 @@ void naturals(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	while  (i < 102)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("%d\n", i);
 		}
+		i++;
 	}
-}
-int main()
-{
-	naturals(1024);
 	return (0);
 }
