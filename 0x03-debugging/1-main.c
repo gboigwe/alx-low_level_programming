e68ff65b99a61a319912044b37b98d23e40f88ad
gboigwe <geakande@gmail.com>
2023-10-12 02:46:31 -0700

    Debugged

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
         i = 0;
-
+/*
 	while (i < 10)
 	{
-		putchar(i); /* i is not assigned a value or task so the code will keep looping */
+		putchar(i);
 	}
-
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
