1795f8005ab581626c23b7cfeab97967d9b27a97
gboigwe <geakande@gmail.com>
2023-10-16 16:17:01 -0700

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - Reverses whatever is been print on to the screen
+ * @s : The string to be checked
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = _strlen(s);
+	for (i = n-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
