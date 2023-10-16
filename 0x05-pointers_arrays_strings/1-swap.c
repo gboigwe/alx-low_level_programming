cac67d35ced15968c0c6f59313dc712f2a7d9e09
gboigwe <geakande@gmail.com>
2023-10-16 14:45:34 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int -This function swaps the value of the pointers
+ * @a: The character to be checked
+ * @b: The character to be checked
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
