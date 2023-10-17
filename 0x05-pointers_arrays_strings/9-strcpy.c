306e1eac7507efcb2eba1c23513fd490877b4610
gboigwe <geakande@gmail.com>
2023-10-16 17:27:47 -0700

    Hello

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - This is a function that copies the string
+ * @dest: destinaton
+ * @src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	while (n >= 0)
+	{
+		*(dest + n) = *(src + n);
+		if (*(src + n) == '\0')
+		{
+			break;
+		}
+		n++;
+	}
+	return (dest);
+}
