eea684a993d38f02bb8dbc71a4c57e5dd80f5d23
gboigwe <geakande@gmail.com>
2023-10-16 17:07:48 -0700

    Cool

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half
+ */
+void puts_half(char *str)
+{
+	int n = 0, i;
+
+	while (n >= 0)
+	{
+		if (str[n] == '\0')
+			break;
+		n++;
+	}
+
+	if (n % 2 == 1)
+		i = n / 2;
+	else
+		i = (n - 1) / 2;
+
+	for (i++; i < n; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
