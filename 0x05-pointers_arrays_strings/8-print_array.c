7ab8d92bb0c7e7e2d88732636ed736899c0830db
gboigwe <geakande@gmail.com>
2023-10-16 17:20:49 -0700

    Doing

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers.
  * @a: input array.
  * @n: input n elements
- * Return: no return.
+ * Return: no
  */
 void print_array(int *a, int n)
 {
