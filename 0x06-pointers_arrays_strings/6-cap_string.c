e96d9f8dc981ab2494dfb6fbf2b633cc667a48e8
gboigwe <geakande@gmail.com>
2023-10-19 16:13:23 -0700

    Move

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,22 +10,22 @@ char *cap_string(char *cap)
 {
 	int i = 0;
 	int j;
-	char badBoys[] = " \n\t,;.!?\"(){}";
+	char charac[] = " \n\t,;.!?\"(){}";
 
-	if (z[0] >= 'a' && cap[0] <= 'z')
+	if (cap[0] >= 'a' && cap[0] <= 'z')
 		cap[0] -= 32;
 
 	for (; cap[i] != '\0'; i++)
 	{
 		for (j = 0; j < 14; j++)
 		{
-			if (cap[i] == badBoys[j])
+			if (cap[i] == charac[j])
 			{
-				if (cap[i + 1] >= 'a' && z[i + 1]  'z')
-					z[i + 1] -= 32;
+				if (cap[i + 1] >= 'a' && cap[i + 1] <= 'z')
+					cap[i + 1] -= 32;
 			}
 		}
 	}
 
-	return (z);
+	return (cap);
 }
