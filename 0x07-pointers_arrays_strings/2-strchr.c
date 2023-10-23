18a57cb999ceb3bac97ab674d64a930073b5d86c
gboigwe <geakande@gmail.com>
2023-10-23 15:29:41 -0700

    CHR

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - This function returns a pointer
+ *
+ * @s: The character to be written
+ * @c: The character to be watched
+ *
+ * Return: Returns NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return ((char *) s);
+		}
+		s++;
+	}
+	return (NULL);
+}
