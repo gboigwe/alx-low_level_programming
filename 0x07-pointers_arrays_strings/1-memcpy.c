20e808ed268391d2b9ec744862d555c9c8666c72
gboigwe <geakande@gmail.com>
2023-10-23 14:43:51 -0700

    Mem cpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,18 @@
  * @src: The source variable
  * @n: The number of n block
  *
- * Return:This function does not return anything
+ * Return: This functio returns destin
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int num = n;
+	unsigned int i;
 	char *source = src;
 	char *destin = dest;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < num; i++)
 	{
 		destin[i] = source[i];
 	}
+	return (dest);
 }
