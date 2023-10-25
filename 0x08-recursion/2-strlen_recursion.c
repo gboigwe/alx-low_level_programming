da77e67b6359a8e3b99ee830bd3537c940e98b7b
gboigwe <geakande@gmail.com>
2023-10-25 11:58:52 -0700

    String Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - This function pints the number of characters
+ * @s: The character to be written
+ *
+ * Return: String length is to be returned
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
