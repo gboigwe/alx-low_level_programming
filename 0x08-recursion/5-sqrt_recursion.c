27a4f8cd1860250261a78395bbf76754b37d4d79
gboigwe <geakande@gmail.com>
2023-10-25 20:35:50 -0700

    Sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - This function finds the square root
+ * @n: The character to be written
+ *
+ * Return: Returning the found square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
+/**
+ * sqrt_recursion_2 - This function calculates the root
+ * @x: The character to be written
+ * @y: The character to be written
+ *
+ * Return: Squared root returned
+ */
+int sqrt_recursion_2(int x, int y)
+{
+	if (y * y == x)
+	{
+		return (y);
+	}
+	else if (y * y > x)
+	{
+		return (-1);
+	}
+	return (sqrt_recursion_2(x, y + 1));
+}
