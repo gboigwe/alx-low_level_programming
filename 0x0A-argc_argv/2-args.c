02f8afdecc0ffeb947e2f52154fecb708dd2a41e
gboigwe <geakande@gmail.com>
2023-10-27 15:16:38 -0700

    All mandatory

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - This function takes in an argument
- * @argc: Parameter that counts command
- * @argv: One-dimensional array parameter
- *
- * Return: 0 Always;
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: One dimensional array name
+ * Return: 0 always
  */
 int main(int argc, char *argv[])
 {
-	int count;
+	int i;
 
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - program that adds positive numbers.
+ * @argc: number of arguments
+ * @argv: One dimensional array with the arguments
+ *
+ * Return: always 0
+ **/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, res = 0;
+	char c[] = "Error", *if_letter;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if_letter = argv[i];
+			while (*if_letter != 0)
+			{
+				if (*if_letter < 47 || *find_letter > 57)
+				{
+					printf("%s\n", c);
+					return (1);
+				}
+				if_letter++;
+			}
+			res = atoi(argv[i]);
+			sum += res;
+		}
+		printf("%d\n", suma);
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}
