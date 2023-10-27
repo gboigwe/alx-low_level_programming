9bc86c54863a0cbe10a5ca8f4705ee8e41e9b3b2
gboigwe <geakande@gmail.com>
2023-10-27 14:56:58 -0700

    cmmit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - This function takes in an argument
+ * @argc: Parameter that counts command
+ * @argv: One-dimensional array parameter
+ *
+ * Return: 0 Always;
+ */
+int main (int argc, char *argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	else
+	{
+		printf("The command had no other arguments.\n");
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * @argc: incoming parameter argument
+ * @argv: incoming parameter argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+    int tot;
+
+    if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	tot = (int) *argv[1] * (int) *argv[2];
+	printf("%d\n", tot);
+	return (0);
+}
