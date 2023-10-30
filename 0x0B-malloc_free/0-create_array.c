058d2652ce7c8a4a9cf68559eeb25c3a0d93575f
gboigwe <geakande@gmail.com>
2023-10-30 16:37:59 -0700

    git

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_array - Function that creates an array of chars.
+ * @size: Th number of allocated memory
+ * @c: The character to be written
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (p != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			p[i] = c;
+		}
+	}
+	return (p);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is a README file that contains ALX project task on malloc memory allocator
