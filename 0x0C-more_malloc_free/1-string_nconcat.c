a42b444ceabafeccbfa69cb0e06272a47554300d
gboigwe <geakande@gmail.com>
2023-11-02 14:19:49 -0700

    All

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,24 +15,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = n, i;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	for (i = 0; s1[i]; i++)
+	{
 		len++;
+	}
 
 	concats = malloc(sizeof(char) * (len + 1));
 
 	if (concats == NULL)
+	{
 		return (NULL);
+	}
 
 	len = 0;
 
 	for (i = 0; s1[i]; i++)
 	{
-		concat[len++] = s1[i];
+		concats[len++] = s1[i];
 	}
 
 	for (i = 0; s2[i] && i < n; i++)
