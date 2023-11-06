9fd971e5bad5f5de69922460065c7acf6afa8648
gboigwe <geakande@gmail.com>
2023-11-06 06:54:44 -0800

    dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - Initializes the structure of the dogs
+ * @d: Pointer that holds the address
+ * @name: Name of dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
