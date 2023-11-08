a71fc9d3f4f0d543aeef51762b6da122740b0e7d
gboigwe <geakande@gmail.com>
2023-11-07 16:22:28 -0800

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-/*
- * File: dog.h
- * Auth: Gbolahan Akande
- * Desc: Header file that defines a new type struct dog.
- */
-
 /**
  * dog - Struct for dog
  * @name: The name of the dog
@@ -27,8 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
 
 #endif
