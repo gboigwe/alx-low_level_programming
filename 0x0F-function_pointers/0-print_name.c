52f3473ed305a44acb1d0e8a50b01c4ef4252441
gboigwe <geakande@gmail.com>
2023-11-08 04:57:06 -0800

    func

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Takes in several functions to print name
+ * @name: The character to be considered
+ * @f: The functions pointer to be considered
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This is a README file contains project tasks that executes functions pointer
