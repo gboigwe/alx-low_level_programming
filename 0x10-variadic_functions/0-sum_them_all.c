484871481fcdb0c7a8d9301cea101d2e1030e75e
gboigwe <geakande@gmail.com>
2023-11-09 12:45:51 -0800

    Var

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Sums up as many value as given
+ * @n: The initialization of the many input
+ * @...: The many input
+ *
+ * Return: The result is returned
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list sum_them;
+	va_start(sum_them, n);
+	unsigned int i;
+	int result = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			result = result + va_arg(sum_them, unsigned int);
+		}
+	}
+	va_end(sum_them);
+	return (result);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
