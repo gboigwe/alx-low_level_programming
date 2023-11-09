ccc123f31d39e1a874a5757af0a066f62716fecb
gboigwe <geakande@gmail.com>
2023-11-08 16:03:54 -0800

    Pushing

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers"
+/**
+ * int_index - Function that searches for an iteger
+ * @array: Array parameter to be taken
+ * @size: Size of the integer in question
+ * @cmp: Function pointer himself
+ *
+ * Return: Returning i
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+				{
+					return (i);
+				}
+			}
+		}
+	}
+}
