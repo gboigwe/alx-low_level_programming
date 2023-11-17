1914eabb421e53b54447a4c6459870a634567ea8
gboigwe <geakande@gmail.com>
2023-11-17 13:55:49 +0100

    push

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * home - I bore my house on my back
+ *
+ * Return: No return
+ */
+void __attribute__ ((constructor)) home(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
