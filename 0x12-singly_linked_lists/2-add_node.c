9c1aa00d97ff52cebee72bed8e214da65b4ab072
gboigwe <geakande@gmail.com>
2023-11-17 10:36:44 +0100

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_note - Adds to the list
+ * @head: Pointer pointing to next double pointer
+ * @str: String to be considered in adding
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t temp;
+	size_t number =  0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	while (str[number])
+	{
+		number++;
+	}
+	temp->len = number;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
