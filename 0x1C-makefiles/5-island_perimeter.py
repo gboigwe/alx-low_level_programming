cbc5750bffd00ad4fd809148fda1eba1935d5955
gboigwe <geakande@gmail.com>
2024-03-26 13:15:27 +0100

    Found the perimeter of island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Finding the perimeter of a land
+Around a sea(island)
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the land
+    Args:
+    grid : list of list of integers
+    """
+    
+    row = len(grid[0])
+    col = len(grid)
+    peri = 0
+    size = 0
+    
+    for i in range(col):
+        for j in range(row):
+            if grid[i][j] == 1:
+                size += 1
+                # Noting boundary or a lake
+                if (j > 0 and grid[i][j - 1] == 1):
+                    peri += 1
+                    if (i > 0 and grid[i - 1][j] == 1):
+                        peri += 1
+                        return size * 4 - peri * 2
